d972c7d8a2ee2c3a18a5b874e43013f72ffbdafe
nthrongtrair <nathakorn.throngtrairat@agoda.com>
2024-08-27 17:41:27 +0700

    update

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -3,8 +3,11 @@ use utils_rs::derive::WithChain;
 #[derive(Debug, Default, WithChain)]
 struct Test {
     name: String,
+    age: i32,
 }
 
 fn main() {
-    println!("Hello, world!");
+    let mut t = Test::default();
+    t = t.with_name("Boss").with_age(39);
+    println!("Hello, world! {:?}", t);
 }
--- utils_derive/src/with_chain.rs
+++ utils_derive/src/with_chain.rs
@@ -1,17 +1,22 @@
 use proc_macro::TokenStream;
-use quote::{format_ident, quote};
-use syn::DeriveInput;
+use quote::{format_ident, quote, quote_spanned};
+use syn::{spanned::Spanned, DeriveInput};
 
 pub fn expand_with_chain(input: DeriveInput) -> TokenStream {
     let item_ident = input.ident;
     let with_fns = match input.data {
         syn::Data::Struct(str) => str.fields.into_iter().filter_map(|f| {
-            if let Some(ident) = f.ident {
+            if let Some(ident) = &f.ident {
                 let fn_name = format_ident!("with_{ident}");
-                let ty = f.ty;
+                let ty = &f.ty;
+
+                let span = f.span();
+                let assign_expand = quote_spanned! {span=>
+                    self.#ident = val.into();
+                };
                 Some(quote! {
-                    pub fn #fn_name(mut self, val: #ty) -> Self {
-                        self.#ident = val;
+                    pub fn #fn_name(mut self, val: impl Into<#ty>) -> Self {
+                        #assign_expand
                         self
                     }
                 })
